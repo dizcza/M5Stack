59e5794ebe321b1f89fe0a0bb9f01019d213b44a
dizcza <dizcza@gmail.com>
2024-02-03 11:48:47 +0200

    ttgo v1.2

--- src/M5StX.cpp
+++ src/M5StX.cpp
@@ -24,7 +24,7 @@ void M5StX::begin(bool SDEnable, bool SerialEnable, bool LCDEnable, bool externa
   #elif defined (ARDUINO_M5STACK_FIRE)
     Wire.begin(21, 22, 400000);
     Power.begin();
-  #elif defined (ARDUINO_FROG_ESP32)  //K46
+  #elif defined (ARDUINO_FROG_ESP32) || defined (ARDUINO_TTGO_T1)  //K46
     Wire.begin(21, 22, (uint32_t) 400000);
     Bat.begin();
   #elif defined (ARDUINO_WESP32)      // K46v1
