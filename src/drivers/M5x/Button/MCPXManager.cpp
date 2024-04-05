d43768b5142bb0cbaf0886e95433c6256fc7c1c5
dizcza <dizcza@gmail.com>
2024-04-05 08:46:12 +0300

    enableTFT_BL fix

--- src/drivers/M5x/Button/MCPXManager.cpp
+++ src/drivers/M5x/Button/MCPXManager.cpp
@@ -29,6 +29,7 @@ std::pair<uint16_t, uint16_t> MCPXManager::read() {
 
 
 void MCPXManager::configurePins() {
+    // 1 - input, 0 - output
     mcpx.pinMode(MCP_EXPANDER_LORA_RST_PIN, 0);
     mcpx.pinMode(MCP_EXPANDER_BTN_A_PIN, 1);
     mcpx.pinMode(MCP_EXPANDER_BTN_B_PIN, 1);
@@ -78,5 +79,5 @@ void MCPXManager::enableTFT()
 }
 
 void MCPXManager::enableTFT_BL(bool enable) {
-    mcpx.pinMode(MCP_EXPANDER_TFT_BL_PIN, enable);
+    mcpx.digitalWrite(MCP_EXPANDER_TFT_BL_PIN, enable);
 }
